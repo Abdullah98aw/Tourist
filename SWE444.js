bbd1815c5e13ee26dbb5321802339908ad60e1d7
abdullah hussain <eng.abdullah_98@outlook.sa>
2020-07-12 20:13:04 +0300

    new edit

--- SWE444.js
+++ SWE444.js
@@ -1,34 +1,10 @@
-let sendButton = document.getElementById('send');
-let resetButton = document.getElementById('reset');
-let form = document.getElementById('form');
-
-
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-})
-
-resetButton.addEventListener('click', function(){
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let message = document.getElementById('message');
-
-    name.value = '';
-    email.value = '';
-    message.value = '';
-})
-
-sendButton.addEventListener('click', function(e){
-    let name = document.getElementById('name');
-    let email = document.getElementById('email');
-    let message = document.getElementById('message');
-
-    name = name.value;
-    localStorage.setItem('name', name);
+function abd1(){
     
-    email = email.value;
-    localStorage.setItem('email', email);
-
-    message = message.value;
-    localStorage.setItem('message', message);
+ document.querySelector("#name").value= " Abdullah Hussain Alawad " ;
+ document.querySelector("#number").value = " 201605774 ";
+}
+function abd2(){
     
-})
+document.querySelector("#name").value= " Hassan Abdulgader Hassan " ; 
+document.querySelector("#number").value = " 201316322 " ;
+}
